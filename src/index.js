3008dede835ce4d9d3fcaaccf3df8734552c4463
k-nasa <htilcs1115@gmail.com>
2019-04-15 16:21:37 +0900

    feat: jsからログイン済みか渡す

--- src/index.js
+++ src/index.js
@@ -1,9 +1,30 @@
 import './main.css';
-import { Elm } from './Main.elm';
+import {Elm} from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
+import * as F from './firebase';
 
-Elm.Main.init({
-  node: document.getElementById('root')
+const storageKey = 'uid';
+const getUidByStorage = storageKey => localStorage.getItem(storageKey);
+
+var loggedIn = getUidByStorage(storageKey) !== null;
+F.init();
+
+if (!loggedIn) {
+  F.fetchFirebaseRedirectResult();
+}
+
+const app = Elm.Main.init({
+  flags: loggedIn,
+  node: document.getElementById('root'),
+});
+
+console.log(app);
+app.ports.signInWithGoogle.subscribe(function() {
+  F.signInWithGoogle();
+});
+
+app.ports.signInWithGitHub.subscribe(function() {
+  F.signInWithGithub();
 });
 
 registerServiceWorker();
