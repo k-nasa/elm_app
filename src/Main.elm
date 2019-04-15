a21c827e89e09b42b44f9eb49bdfb82761f00776
k-nasa <htilcs1115@gmail.com>
2019-04-15 14:15:26 +0900

    feat: basic navigation

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,7 @@ import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Route exposing (Route, parse)
 import Url exposing (Url)
 
 
@@ -21,15 +22,25 @@ main =
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( {}, Cmd.none )
+    -- どうせgoToでmodel.pageは書き換わるのでNotFoundで仮置き
+    Model NotFound key
+        |> goTo (Route.parse url)
 
 
 
 ---- MODEL ----
 
 
+type Page
+    = NotFound
+    | TopPage
+    | AboutPage
+
+
 type alias Model =
-    {}
+    { page : Page
+    , key : Nav.Key
+    }
 
 
 
@@ -38,11 +49,41 @@ type alias Model =
 
 type Msg
     = NoOp
+    | LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            goTo (Route.parse url) model
+
+        _ ->
+            ( model, Cmd.none )
+
+
+goTo : Maybe Route -> Model -> ( Model, Cmd Msg )
+goTo maybeRoute model =
+    case maybeRoute of
+        Nothing ->
+            ( { model | page = NotFound }, Cmd.none )
+
+        Just Route.Top ->
+            ( { model | page = TopPage }
+            , Cmd.none
+            )
+
+        Just Route.About ->
+            ( { model | page = AboutPage }, Cmd.none )
 
 
 
