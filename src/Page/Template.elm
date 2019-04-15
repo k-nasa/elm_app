cd36bab541daba9de8ef7e0b23ac26e426c38b53
k-nasa <htilcs1115@gmail.com>
2019-04-15 21:05:43 +0900

    change favicon

---
+++ src/Page/Template.elm
@@ -0,0 +1,23 @@
+module HogeHoge exposing (Model, Msg(..), init, view)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+init : Model
+init =
+    {}
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = NoOp
+
+
+view : Model -> Html Msg
+view model =
+    div [] []
