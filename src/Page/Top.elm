a912b171b9a284fbbd9a91dc54ff167eb2c8d305
k-nasa <htilcs1115@gmail.com>
2019-04-16 11:18:19 +0900

    feta: TopPageの雛形を作成

--- src/Page/Top.elm
+++ src/Page/Top.elm
@@ -27,4 +27,35 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div [] [ text "top" ]
+    div [ id "top-page" ]
+        [ viewSideBar
+        , viewContainer
+        ]
+
+
+viewContainer : Html Msg
+viewContainer =
+    div [ class "top-page-container" ]
+        [ a [] [ text "学習を始める" ]
+        , p [] [ text "学習すべきカード" ]
+        , div [] [ text "新しいカード" ]
+        , div [] [ text "復習すべきカード" ]
+        , div [ class "footer-button" ] [ a [] [ text "学習カードを追加" ] ]
+        ]
+
+
+viewSideBar : Html Msg
+viewSideBar =
+    div [ class "sidebar" ]
+        [ div [ class "sidebar-menue" ]
+            [ ul []
+                [ li [] [ a [] [ text "カードを編集" ] ]
+                , li [] [ a [] [ text "学習記録" ] ]
+                , li [] [ a [] [ text "設定" ] ]
+                , li [] [ a [] [ text "ヘルプ" ] ]
+                , li [] [ a [] [ text "ログアウト" ] ]
+                , li [] [ a [] [ text "要望" ] ]
+                , a [ href "https://twitter.com/intent/tweet?text=%E3%82%A2%E3%83%B3%E3%82%AD%E3%83%91%E3%83%B3%E3%81%A0%E3%81%8A", target "_bulk" ] [ text "ツイート" ]
+                ]
+            ]
+        ]
