43188ff085219946ef12566d60104deb3cbae4b4
k-nasa <htilcs1115@gmail.com>
2019-05-04 13:54:08 +0900

    feat: Add cacheCards port
    Cardsをlocal storageに入れて取得の手間を省く

--- src/Port.elm
+++ src/Port.elm
@@ -1,4 +1,6 @@
-port module Port exposing (clearLocalStorageUid, loading, receivedLoggedIn, signInWithGitHub, signInWithGoogle)
+port module Port exposing (cacheCards, clearLocalStorageUid, loading, receivedLoggedIn, signInWithGitHub, signInWithGoogle)
+
+import Data.Card exposing (Cards)
 
 
 port signInWithGoogle : () -> Cmd msg
@@ -14,3 +16,6 @@ port receivedLoggedIn : (() -> msg) -> Sub msg
 
 
 port loading : (Bool -> msg) -> Sub msg
+
+
+port cacheCards : Cards -> Cmd msg
